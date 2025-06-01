64b97149dcd40320b2417b3307c8650555ba082c
cravingfoodie <cravingsomefoodie@gmail.com>
2025-06-02 02:09:27 +0700

    Update ecommerce-price-men logged.js

--- JS_files/ecommerce-price-men logged.js
+++ JS_files/ecommerce-price-men logged.js
@@ -107,49 +107,6 @@ $(document).on('click', '.next', function () {
 });
 
 
-//for single slider
-// $('.single-slider').slick({
-//     dots: false,
-//     infinite:false,
-//     autoplaySpeed: 300,
-//     slidesToShow: 4,
-//     slidesToScroll: 1,
-//     responsive: [
-//       {
-//         breakpoint: 1200,
-//         settings: {
-//           slidesToShow: 3,
-//           slidesToScroll: 1,
-//           infinite: true,
-//           arrows: false,
-//           dots: false
-//         }
-//       },
-//       {
-//         breakpoint: 991,
-//         settings: {
-//           slidesToShow: 2,
-//           slidesToScroll: 1,
-//           infinite: true,
-//           dots: false,
-//           arrows: false
-//         }
-//       },
-//       {
-//         breakpoint: 576,
-//         settings: {
-//           slidesToShow: 1,
-//           slidesToScroll: 1,
-//           infinite: true,
-//           dots: false,
-//           arrows: false
-//         }
-//       }
-//       // You can unslick at a given breakpoint now by adding:
-//       // settings: "unslick"
-//       // instead of a settings object
-//     ]
-//   }); 
   //on scroll plugin//
   AOS.init({
     once:true,
